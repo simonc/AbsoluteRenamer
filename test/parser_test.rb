f914991cc5d26cef64b88063abaaf52315b27011
Simon COURTOIS <happynoff@free.fr>
2009-10-04 12:23:18 +0200

    Loading the externals in the Parser tests

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -3,9 +3,11 @@ require 'test_helper'
 class ParserTest < Test::Unit::TestCase
   context "A Parser instance" do
 
-    context "with config file loaded and the -f'format' option set" do
+    context "with config file and core-externals loaded and the -f'format' option set" do
 
       setup do
+        AbsoluteRenamer::External.load_core
+
         AbsoluteRenamer::Config.load('conf/absrenamer/absrenamer.conf')
         ARGV << '-f'
         ARGV << 'format'
