d472f833d34afc52532676d8a48902ceb5dba5c7
Simon COURTOIS <happynoff@free.fr>
2009-10-05 16:11:25 +0200

    Removing the camelize word_separators parameter and removing it from the conf

--- lib/absolute_renamer/libs/string.rb
+++ lib/absolute_renamer/libs/string.rb
@@ -4,18 +4,17 @@ class String
     # word_separator: a regular expression used to separate words.
     #    str = "hello.THE world"
     #    str.camelize           # => "Hello.The World"
-    #    str.camelize(/[\.]/)   # => "Hello.The world"
     #    str                    # => "hello.THE world"
-    def camelize(word_separators = /[\W_]/)
-        self.clone.camelize!(word_separators)
+    def camelize
+        self.clone.camelize!
     end
 
     # Camelizes a string and returns it.
     #    str = "Hello.THE World"
     #    str.camelize!          # => "Hello.The World"
-    #    str.camelize!(/[\.]/)  # => "Hello.The World"
     #    str                    # => "Hello.The World"
-    def camelize!(word_separators = /[\W_]/)
+    def camelize!
+        word_separators = /[^a-zA-Z0-9']/
         self.downcase!
         self.each_char.each_with_index do |c,i|
             if self[i-1].chr =~ word_separators or i.zero?
